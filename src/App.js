55634a153c71de6e51864125c9412333bb2704a2
Alibek <freed.alibek@gmail.com>
2023-01-08 01:02:37 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./styles/default.css";
+import stepsData from "./stepsData.json"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<main className="food">
+				<article className="food__article">
+					<h1 className="food__title">How does it work?</h1>
+					<p className="food__text">Producers, who don’t want to waste their products can sell it at a discount price and consumers canbuy it</p>
+					<a href="#some-link" className="food__button">Get started<img src={process.env.PUBLIC_URL + "/images/arrow.svg"} alt="arrow" /></a>
+				</article>
+				<img className="food__img" src={process.env.PUBLIC_URL + "/images/pyramid.png"} alt="food-pyramid" />
+			</main>
+			<ol className="list">
+				{
+					stepsData.map((item, index) => {
+						return (
+							<li className="list__item" key={index}>
+								<p className="list__number">{index + 1}</p>
+								<h2 className="list__title">{item.title}</h2>
+								<p className="list__description">{item.description}</p>
+							</li>
+						)
+					})
+				}
+			</ol>
+		</>
+	);
 }
 
 export default App;
